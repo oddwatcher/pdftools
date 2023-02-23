df40f3ea2582a0501720bfe65395125ba5cbf7f9
oddwatcher <science-35@outlook.com>
2023-02-23 21:11:16 +0800

    fixed the depercation of pypdf2

--- doubleprint.py
+++ doubleprint.py
@@ -10,38 +10,38 @@ while (True):
         except:
             print("file not exist")
             din = input("The PDF to process")
-    pdfin = pdf.PdfFileReader(din)
-    lastpage = pdf.PdfFileReader("./lastpage.pdf")
-    pdfodd = pdf.PdfFileWriter()
-    pdfeven = pdf.PdfFileWriter()
-    total = pdfin.numPages
+    pdfin = pdf.PdfReader(din)
+    lastpage = pdf.PdfReader("./lastpage.pdf")
+    pdfodd = pdf.PdfWriter()
+    pdfeven = pdf.PdfWriter()
+    total = len(pdfin.pages)
     for i in range(0, total, 2):
-        page = pdfin.getPage(i)
-        pdfeven.addPage(page)
+        page = pdfin.pages[i]
+        pdfeven.add_page(page)
     
     if (flag == '1'):
         if total % 2 != 0:
             tail = total-2
-            pdfodd.addPage(lastpage.getPage(0))
+            pdfodd.add_page(lastpage.pages[0])
         else:
             tail = total-1
         for i in range(tail, 0, -2):
-            page = pdfin.getPage(i)
-            pdfodd.addPage(page)
+            page = pdfin.pages[i]
+            pdfodd.add_page(page)
     else:
         for i in range(1, total-1, 2):
-            page = pdfin.getPage(i)
-            pdfodd.addPage(page)
+            page = pdfin.pages[i]
+            pdfodd.add_page(page)
         if (total+1) %2 !=0:
-            pdfodd.addPage(lastpage.getPage(0))
+            pdfodd.add_page(lastpage.pages[0])
     oddout = open(f"./output/{str(f)}second.pdf", 'wb')
     evenout = open(f"./output/{str(f)}first.pdf", 'wb')
     pdfeven.write(evenout)
     pdfodd.write(oddout)
     evenout.close()
     oddout.close()
-    outstr = "Even:%d,Odd:%d,Total:%d,File=%s" % (
-        pdfeven.getNumPages(), pdfodd.getNumPages(), total+1, f)
+    outstr = "Even:%d,Odd:%d,Total:%d,File:%s" % (
+        len(pdfeven.pages), len(pdfodd.pages), total+1, f)
     print(outstr)
     input("press to restart\n")
     f = f+1
