6a7a80f3daa5a39123bd9152daded560b8757923
oddwatcher <science-35@outlook.com>
2024-01-19 07:19:15 +0800

    new feature merger

--- PDFmerger.py
+++ PDFmerger.py
@@ -1,10 +1,30 @@
 import PyPDF2 as pdf
+merger = pdf.PdfWriter()
+
 while True:
+    
+    din = input("files to merge:")
+    din = din.lstrip("\"")
+    din = din.rstrip("\"")
+    print(din)
     try:
-        din = input("files to merge:")
-        PDFin = open(din,'r')
-        break
-    except:
+        open(din,'r')
+        try:
+            reader=pdf.PdfReader(din)
+            num_page=len(reader.pages)
+            for i in range(0,num_page):
+                page = reader.pages[i]
+                merger.add_page(page)
+        except:
+            print("something is wrong with pdf file\n")
+    except: 
+        if(din == "fin"):
+            break
         print("File not found,Retry")
-buff = pdf.PdfFileMerger()
-buff.merge()
+        print("to stop type fin\n")
+        print("current pages:{}\n".format(len(merger.pages)))
+
+
+print("current pages:{}\n".format(len(merger.pages)))
+
+merger.write("./output.pdf")
